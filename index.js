d9be2fe99154de8fafc5b366b08cb5ba09a2676a
AviVashishta29 <avivashishta@gmail.com>
2024-12-17 21:04:36 +0530

    removed node_modules and package-loc

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import { photos } from './photos.js';
 
 const app = express();
 const port = 5001;
 
-
 app.get('/', (req, res) => {
     res.send({ message: "Hello, world!" })
-})
-
-app.get('/photos', (req, res) => {
-    res.send({ data: photos })
 })
 
 app.listen(port, () => {
